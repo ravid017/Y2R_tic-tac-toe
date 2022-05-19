34def5dcee8d45ed091f6ea7535fdb5d202089a9
Ravid <ravid@gmail.com>
2022-05-19 17:37:25 +0300

    working on checking functions

--- game_engine.js
+++ game_engine.js
@@ -1,5 +1,7 @@
 const MIN_BOARD_SIZE = 3;
 const MAX_BOARD_SIZE = 9;
+const size = 3;
+
 function createBoard(size) {
     if (size < MIN_BOARD_SIZE || size > MAX_BOARD_SIZE) {
         size = 3
@@ -8,11 +10,69 @@ function createBoard(size) {
     return board;
 }
 
+let board = createBoard(size);
+
 
 function checkVerticals(board) {
     board.forEach(element => {
-        for (i of board) {
-            i == ++i ? true : false
+        for (i of element) {
+            if (i == ++i ? true : false) { win() }// counter ++, if counter = size ()=>win
+
+            else { break }
+
         }
     });
 }
+table= [[1,2,3]
+        [2,2,2]   
+        [1,2,3] ] 
+
+
+function chek_horizontal(board) {
+    debugger;
+    for (x of table) {
+        for (y of table[x]) {
+            if (y == y + 1) {console.log(`${y}=${y+1}`); }
+            else { break }
+        
+        }
+
+    }
+    console.log("nada");
+}
+
+
+
+
+
+// function check_diagonals_left_to_right(board) {//always true
+//    debugger;
+//     for (let board_index = 0; board_index < board.length; board_index++) {
+//         if (board_index[board_index] == ++board_index[board_index]) {
+//             win();
+//             break;
+//         }
+//         else {
+//             check_diagonals_left_to_right;
+//         }
+//     }
+// }
+
+// function check_diagonals_right_to_left(board){
+//    let banana=0;
+//     for (let board_index = board.length-1; board_index > 0; board_index--) {
+//         // debugger
+//         if (board_index[banana] == --board_index[++banana]) {
+//             console.log("array num: "+banana +"index : " +board_index+" match");
+//         }
+//         else {
+//             console.log(board_index + "no match");
+//         }
+
+//     }
+// }
+
+
+
+function win() { console.log("win!") }
+
